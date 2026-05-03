0226e3aa992fbad667856aa1b2a6c025d0469282
Siyabonga-Zindela <siyabongazindela@outlook.com>
2026-05-03 03:44:24 +0200

    First commit

---
+++ js/index.js
@@ -0,0 +1,73 @@
+
+const apiKey = "7a107e0b667e8bcefecdab5f0efeb9c9";
+let cityName = document.getElementById("search-query");
+const countryCode = "ZA";
+
+async function FetchWeather(cityName){
+   try { 
+        await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName},${countryCode}&limit=1&appid=${apiKey}`)
+            .then((response =>{
+                return response.json()
+            }))
+            .then((coordinatesData =>{
+                let lat = coordinatesData[0].lat;
+                let longitude = coordinatesData[0].lon;
+                try {
+                     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${longitude}&appid=${apiKey}`)
+                        .then((response => {
+                            return response.json();
+                        }))
+                        .then((weatherData => {
+                            let feelsLike = (weatherData.main.feels_like - 273.15).toFixed(0);
+
+                            let maxTemp = (weatherData.main.temp_max - 273.15).toFixed(0);
+                            let minTemp = (weatherData.main.temp_min - 273.15).toFixed(0);
+                            let weatherDescription = weatherData.weather[0].description;
+                            let weatherIcon = weatherData.weather[0].icon;
+                            let cityName = weatherData.name;
+
+                            //wind conditions
+                            let windSpeed = (weatherData.wind.speed * 3.6).toFixed(2);
+                            let gust = (weatherData.wind.gust * 3.6).toFixed(2);
+                            let pressure = weatherData.main.pressure;
+                            
+                            let city = document.getElementById("city-name");
+                            city.textContent = cityName;
+
+                            let weather_Description = document.getElementById("description");
+                            weather_Description.textContent = weatherDescription +  " | " +"Feels like "+ feelsLike;
+
+                            let max_temp = document.getElementById("maxTemp");
+                            max_temp.textContent = maxTemp +" °C";
+
+                            let min_temp = document.getElementById("minTemp");
+                            min_temp.textContent = minTemp +" °C";
+
+                            let wind_Speed = document.getElementById("windSpeed");
+                            wind_Speed.textContent = windSpeed +" Km/h";
+
+                            let _pressure = document.getElementById("pressure");
+                            _pressure.textContent = pressure +" hpa";
+
+                            let _gust = document.getElementById("gust");
+                            _gust.textContent = gust +" Km/h";
+
+                        }))
+                } catch (error) {
+                    console.log("Unable to fetch weather data. "+ error)
+                }
+               
+            }))
+    } 
+    catch (error) {
+    console.log("Unable to fetch city "+ error)
+    }
+
+} 
+
+let form = document.getElementById("form");
+form.addEventListener("submit",(e =>{
+    e.preventDefault();
+    FetchWeather(cityName.value);
+
+}))
